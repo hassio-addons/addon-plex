d11b8572a5d6ccd539f76022e5ca33c34e92a319
Franck Nijhof <frenck@geekchimp.com>
2018-05-24 01:05:33 +0200

    :hammer: Refactors the onboarding process

--- plex/rootfs/etc/cont-init.d/10-first-run.sh
+++ plex/rootfs/etc/cont-init.d/10-first-run.sh
@@ -9,44 +9,40 @@ source /usr/lib/hassio-addons/base.sh
 readonly prefs="/data/Plex Media Server/Preferences.xml"
 
 function getPref {
-  local key="$1"
+    local key="$1"
 
-  xmlstarlet sel -T -t -m "/Preferences" -v "@${key}" -n "${prefs}"
+    xmlstarlet sel -T -t -m "/Preferences" -v "@${key}" -n "${prefs}"
 }
 
 function setPref {
-  local key="$1"
-  local value="$2"
+    local key="$1"
+    local value="$2"
 
-  count="$(xmlstarlet sel -t -v "count(/Preferences/@${key})" "${prefs}")"
-  count=$((count + 0))
-  if [[ $count -gt 0 ]]; then
-    xmlstarlet ed --inplace --update "/Preferences/@${key}" -v "${value}" "${prefs}"
-  else
-    xmlstarlet ed --inplace --insert "/Preferences"  --type attr -n "${key}" -v "${value}" "${prefs}"
-  fi
+    count="$(xmlstarlet sel -t -v "count(/Preferences/@${key})" "${prefs}")"
+    count=$((count + 0))
+    if [[ $count -gt 0 ]]; then
+        xmlstarlet ed --inplace --update \
+            "/Preferences/@${key}" -v "${value}" "${prefs}"
+    else
+        xmlstarlet ed --inplace --insert \
+            "/Preferences"  --type attr -n "${key}" -v "${value}" "${prefs}"
+    fi
 }
 
 if ! hass.file_exists "${prefs}"; then
-  hass.log.info 'First run! Initializing configuration files...'
+    hass.log.info 'First run! Initializing configuration files...'
 
-  mkdir -p "$(dirname "${prefs}")"
+    if ! hass.config.has_value "claim_code"; then
+        hass.log.fatal "Aborting. A claim code is required!"
+        hass.die "Please check the installation manual of the add-on"
+    fi
 
-  cat > "${prefs}" <<-EOF
-<?xml version="1.0" encoding="utf-8"?>
-<Preferences/>
-EOF
-
-  hass.log.debug "Generating unique serial & client id's..."
-  serial=$(uuidgen)
-  clientId=$(sha1sum <<< "${serial} - Hass.io Plex add-on" | cut -b 1-40)
-  setPref "MachineIdentifier" "${serial}"
-  setPref "ProcessedMachineIdentifier" "${clientId}"
-
-  if hass.config.has_value 'claim_code'; then
-    hass.log.debug "Claiming server with PlexOnline..."
+    hass.log.debug "Generating unique serial & client id's..."
+    serial=$(uuidgen)
+    clientId=$(sha1sum <<< "${serial} - Hass.io Plex add-on" | cut -b 1-40)
     claim_code=$(hass.config.get 'claim_code')
-    loginInfo="$(curl -X POST \
+    if ! response=$(curl --silent --show-error \
+        --write-out '\n%{http_code}' --request POST \
         -H "X-Plex-Client-Identifier: ${clientId}" \
         -H 'X-Plex-Product: Plex Media Server' \
         -H 'X-Plex-Version: 1.1' \
@@ -55,11 +51,39 @@ EOF
         -H 'X-Plex-Platform-Version: 1.0' \
         -H 'X-Plex-Device-Name: PlexMediaServer' \
         -H 'X-Plex-Device: Linux' \
-        "https://plex.tv/api/claim/exchange?token=${claim_code}")"
-    token="$(echo "$loginInfo" | sed -n 's/.*<authentication-token>\(.*\)<\/authentication-token>.*/\1/p')"
-    setPref "PlexOnlineToken" "${token}"
-  fi
+        "https://plex.tv/api/claim/exchange?token=${claim_code}"
+    ); then
+        hass.log.debug "${response}"
+        hass.log.fatal "Something went wrong contacting the Plex API"
+        hass.die "Maybe your claim code is wrong or expired?"
+    fi
 
-  mkdir -p "/share/transcode"
-  setPref "TranscoderTempDirectory" "/share/transcode"
+    status=${response##*$'\n'}
+    response=${response%$status}
+
+    if [[ "${status}" -ne 200 ]]; then
+        hass.log.debug "${response}"
+        hass.log.fatal "Something went wrong contacting the Plex API"
+        hass.die "Maybe your claim code is wrong or expired?"
+    fi
+
+    hass.log.debug "Plex API HTTP Response code: ${status}"
+    hass.log.debug "Plex API Response: ${response}"
+
+    token="$(echo "${response}" | sed -n 's/.*<authentication-token>\(.*\)<\/authentication-token>.*/\1/p')"
+
+    mkdir -p "$(dirname "${prefs}")"
+
+    cat > "${prefs}" <<-EOF
+<?xml version="1.0" encoding="utf-8"?>
+<Preferences/>
+EOF
+
+    setPref "MachineIdentifier" "${serial}"
+    setPref "ProcessedMachineIdentifier" "${clientId}"
+    setPref "PlexOnlineToken" "${token}"
+    setPref "FriendlyName" "Hass.io"
+
+    mkdir -p "/share/transcode"
+    setPref "TranscoderTempDirectory" "/share/transcode"
 fi
