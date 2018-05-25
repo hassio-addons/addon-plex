73c022cf92791e4826f1120751ba85076ba8b909
Franck Nijhof <frenck@geekchimp.com>
2018-05-25 21:22:44 +0200

    :sparkles: Adds support for WebTools

---
+++ plex/rootfs/etc/cont-init.d/20-webtools.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Community Hass.io Add-ons: Plex
+# Enables the WebTools plugin if the user requested that
+# ==============================================================================
+# shellcheck disable=SC1091
+source /usr/lib/hassio-addons/base.sh
+
+if hass.config.true 'webtools' && ! hass.directory_exists \
+        "/data/Plex Media Server/Plug-ins/WebTools.bundle"; then
+    hass.log.info 'Enabling WebTools plugin...'
+    ln -s "/opt/WebTools.bundle" "/data/Plex Media Server/Plug-ins/"
+fi
+
+if hass.config.false 'webtools' && hass.directory_exists \
+        "/data/Plex Media Server/Plug-ins/WebTools.bundle"; then
+    rm -f "/data/Plex Media Server/Plug-ins/WebTools.bundle"
+fi
